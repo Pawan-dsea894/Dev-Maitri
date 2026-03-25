8d114ad52b8db10d17a8a2b2c9d14af18ee0f0ca
Pawan-dsea894 <pawankumar@cagrameen.in>
2026-03-25 15:41:36 +0530

    Delete BackEnd/apz_java_microservices directory

--- BackEnd/apz_java_microservices/AppzillonBanking-CAGL/src/main/java/com/iexceed/appzillonbanking/cagl/collection/service/KendraFetchService.java
+++
@@ -1,5 +0,0 @@
-package com.iexceed.appzillonbanking.cagl.collection.service;
-
-public class KendraFetchService {
-
-}
--- BackEnd/apz_java_microservices/AppzillonBanking-CAGL/src/main/java/com/iexceed/appzillonbanking/cagl/repository/cus/OfficeDataRepo.java
+++
@@ -1,9 +0,0 @@
-package com.iexceed.appzillonbanking.cagl.repository.cus;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.iexceed.appzillonbanking.cagl.entity.OfficeData;
-
-public interface OfficeDataRepo extends JpaRepository<OfficeData, String> {
-
-}
